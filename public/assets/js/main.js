b3eae9f9f40b0537525b897b5641cef1e14891b1
Diego Schell Fernandes <diego.schell.f@gmail.com>
2016-06-06 10:00:39 -0300

    Ajustes login / redes sociais

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,5 +5,13 @@
     $('#signin-link').on('click', function() {
       $('#signin-overlay').addClass('show');
     });
+
+    $(document).on('click', function(e) {
+      if ($(e.target).closest('#signin-link').length) {
+        $("#signin-overlay").addClass('show');
+      } else if (!$(e.target).closest('.login-box').length) {
+        $('#signin-overlay').removeClass('show');
+      }
+    });
   });
 })();
