b3804614af44bac5b310e5cc8381721df0e95e28
Cleiton Souza <cleiton7souza@gmail.com>
2016-06-09 21:13:33 -0300

    fechar com esc

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,6 +5,17 @@
     // abre modal login
     $('.btn-signin').on('click', function () {
       $('.signin').addClass('signin--show');
+
+      // fecha modal de login com esc
+      $(document).one('keypress', function(event) {
+        console.log(event);
+        if (event.keyCode == 27) {
+          var signin = $('.signin.signin--show');
+          if (signin.length) {
+            signin.removeClass('signin--show');
+          }
+        }
+      });
     });
 
     // fecha modal de login clicando no overlay ou no x
@@ -12,17 +23,6 @@
       var target = $(event.target);
       if (target.hasClass('signin')) {
         target.removeClass('signin--show');
-
-        // fecha modal de login com esc
-        $(document).one('keypress', function(event) {
-          console.log(event);
-          if (event.keyCode == 27) {
-            var signin = $('.signin.signin--show');
-            if (signin.length) {
-              signin.removeClass('signin--show');
-            }
-          }
-        });
       } else if (target.hasClass('btn-close') || target.parent().hasClass('btn-close')) {
         $('.signin').removeClass('signin--show');
       }
